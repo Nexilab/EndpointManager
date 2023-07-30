3044acda1e6315a35bae57e6b20bcfd01936ebfe
git <eae966@gmail.com>
2023-07-31 03:00:47 +0330

    Some Updates.

--- app/api/v1/publicApi.py
+++ app/api/v1/publicApi.py
@@ -10,17 +10,24 @@ router = APIRouter()
 
 @router.post("/publicApi/{blockchain}")
 def sendReq(blockchain: str, payload: dict = Body(...)):
-    rpcUrl = getRandomRpcUrl(blockchain)
-    print(f"Selected rpcurl is : {rpcUrl}")
-    headers = {'Content-Type': 'application/json'}
+    max_retries = 500
+    retry_count = 0
 
-    try:
-        response = requests.post(rpcUrl, json=payload, headers=headers, timeout=(30, 30))
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
-    except json.JSONDecodeError as e:
-        print(f"Failed to parse JSON data: {e}")
-        raise HTTPException(status_code=400, detail="Invalid JSON data!")
+    while retry_count < max_retries:
+        rpcUrl = getRandomRpcUrl(blockchain)
+        print(f"Selected rpcurl is : {rpcUrl}")
+        headers = {'Content-Type': 'application/json'}
+
+        try:
+            response = requests.post(rpcUrl, json=payload, headers=headers, timeout=(30, 30))
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            retry_count += 1
+        except json.JSONDecodeError as e:
+            print(f"Failed to parse JSON data: {e}")
+            raise HTTPException(status_code=400, detail="Invalid JSON data!")
+
+    # If max retries reached and no successful response, raise an error
+    raise HTTPException(status_code=500, detail="Failed to get a valid response after multiple retries.")
